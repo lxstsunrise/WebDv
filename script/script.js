a393886fbb55d40bdddd36b31aebcba41b507c43
Sergei <ghost.rev2010@yandex.ru>
2022-02-01 19:26:28 +0300

    h5

--- script/script.js
+++ script/script.js
@@ -17,3 +17,42 @@ $(document).mouseup(function(e) {
         container.hide();
     }
 });
+//for product menu1
+$(function() {
+    $('.options__button1').click(function() {
+        $('.nav__option-color').slideToggle();
+    });
+});
+
+$(document).mouseup(function(e) {
+    var container = $(".nav__option-color");
+    if (container.has(e.target).length === 0) {
+        container.hide();
+    }
+});
+//for product menu2
+$(function() {
+    $('.options__button2').click(function() {
+        $('.nav__option-size').slideToggle();
+    });
+});
+
+$(document).mouseup(function(e) {
+    var container = $(".nav__option-size");
+    if (container.has(e.target).length === 0) {
+        container.hide();
+    }
+});
+//for product menu3
+$(function() {
+    $('.options__button3').click(function() {
+        $('.nav__option-quant').slideToggle();
+    });
+});
+
+$(document).mouseup(function(e) {
+    var container = $(".nav__option-quant");
+    if (container.has(e.target).length === 0) {
+        container.hide();
+    }
+});
